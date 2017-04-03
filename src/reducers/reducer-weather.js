2ed56283283d76991c6b69641ba1382aa7860506
JudyVue <judyvue87@gmail.com>
2017-04-03 14:57:31 -0700

    starting lesson 61

--- src/reducers/reducer-weather.js
+++ src/reducers/reducer-weather.js
@@ -1,4 +1,12 @@
-export default function(state = null, action){
-  
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action){
+  switch(action.type){
+  case FETCH_WEATHER:
+    //we never want to manipulate our state, so that's why we're using spread operator to create entirely new array and new state
+    return [ action.payload.data, ...state ]; //[city, city, city] NOT [city, [city, city]]
+  }
+
+
   return state;
 }
