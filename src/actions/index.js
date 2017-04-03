0aacadbd45c8170b3378598f993a44a3772652e0
JudyVue <judyvue87@gmail.com>
2017-04-03 14:43:56 -0700

    starting lesson 60

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,18 @@
+import axios from 'axios';
+
 const API_KEY = '4a9f6816d6628089f58c881d86182195';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+//user enters search term, they submit the form that calls action creator, we craft URL with city and make GET request with Axios. Axios returns a promise to us.
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request,
+  };
+}
--- src/containers/search-bar.js
+++ src/containers/search-bar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super (props); //this is like inheritance
     this.state = {term: ''};
 
     this.onInputChange = this.onInputChange.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
     //this constructor is an instance of SearchBar, we define a function of onInputChange, we bind it to that the "this" context is bound to the context of this constructor scope
   }
 
@@ -18,6 +22,8 @@ export default class SearchBar extends Component {
     event.preventDefault();
 
     //We need to go and fetch weather data
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
 
   render(){
@@ -35,3 +41,10 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch){
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+//by binding the action creater fetchWeather to dispatch and mapping it to props, we have access to fn called this.props.fetchWeather in this component
+export default connect(null, mapDispatchToProps)(SearchBar);
